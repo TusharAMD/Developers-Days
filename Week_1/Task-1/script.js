62c1178ee06d43b778c397dfd3e72aed1be1a45d
Tushar <tusharvamdoskar#gmail.com>
2021-09-04 12:46:49 +0530

    6

--- Week_1/Task-1/script.js
+++ Week_1/Task-1/script.js
@@ -3,3 +3,11 @@ function myFunction() {
   console.log("not working");
   console.log("working 100%");
 }
+
+function calc(input){
+    if input == 7
+    {
+         document.getElementById("demo").innerHTML=7;
+    }
+    
+}
